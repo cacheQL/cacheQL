acdfe1c64425ddfea6310fad67f8003accec8cb8
JoshuaManto <joshuamanto@yahoo.com>
2019-12-05 12:29:54 -0800

    Saves information in cachify and removed unnecessary comments

--- redis.js
+++ redis.js
@@ -1,97 +1,98 @@
 const redis = require("redis");
 
-const redisHost = "redis-10212.c52.us-east-1-4.ec2.cloud.redislabs.com";
-const redisPort = process.argv[3] || 10212;
-const redisAuth = "eRQFVq70CXuDEoISTvKNVFtdevWabNbe";
+let redisHost;
+let redisPort;
+let redisAuth;
 
-const client = redis.createClient({
-  port: redisPort,
-  host: redisHost
-});
+let timeToLive;
 
-client.auth(redisAuth, function(err, response) {
-  if (err) {
-    throw err;
-  }
-  console.log("Client Authenticated");
-});
+let client;
 
 const cacheQL = {};
 
-cacheQL.cachify = (req, res, next) => {
+// Sets the redis database/cache configuration
+// User calls the set function and sends:
+// redisHost, redisPort, redisAuth, and/or timeToLive
+
+cacheQL.set = data => {
+  if (data.redisHost) redisHost = data.redisHost;
+  if (data.redisPort) redisPort = data.redisPort;
+  if (data.redisAuth) redisAuth = data.redisAuth;
+  if (data.timeToLive) timeToLive = data.timeToLive;
+};
+
+// Authenticates the redis configuration based on the information set by the user
+
+cacheQL.auth = () => {
+  client = redis.createClient({
+    port: redisPort,
+    host: redisHost
+  });
+
+  client.auth(redisAuth, function(err, response) {
+    if (err) {
+      throw err;
+    }
+    console.log("Client Authenticated");
+  });
+};
+
+cacheQL.checkify = (req, res, next) => {
   // Checks the query if it is inside the cache
-  client.get(res.locals.query, function(err, response) {
+  client.GET(req.body.query, function(err, response) {
+    // req.body.query is the graphql query from the frontend request
     if (err) {
       throw err;
     } else {
       // The query is not inside the cache
+      // redis returns null if the key is not inside the redis database/cache
       if (response === null) {
-        // Need to figure out how to go to db then go back here to save the query and result to cache
-        // Create another function
-        // Which will be called after the endpoint middleware (like database)
-        // Then call said function to check if cache is null
-        // If null then saves qeury and result in cache
         res.locals.cache = null;
+        return next();
       } else {
         // the query is in the cache
-        console.log(response);
         // saves the result in cache variable in res.locals
         // to be accessible in the next middleware
-        res.locals.cache = response;
+        // Use JSON.parse because the response (value from redis) is saved using JSON stringify
+        res.locals.cache = JSON.parse(response);
         return next();
       }
     }
   });
 };
 
-// const cachify = query => {
-//   client.get(query, function(err, response) {
-//     if (err) {
-//       throw err;
-//     } else {
-//       // The query is not inside the cache
-//       if(response === null) {
-//         // Need to figure out how to go to db then go back here to save the query and result to cache
-//       }
-//       else {
-//         // query is in the cache
-//         console.log(response);
+// Need to figure out how to go to db then go back here to save the query and result to cache
+// Create another function (cachify)
+// Which will be called after the endpoint middleware (like database)
+// Then call said function to check if cache is null
+// If null then saves query and result in cache
 
-//       }
-//     }
-//   });
-// };
+cacheQL.cachify = (req, res, next) => {
+  //This is a case where the query doesn't exist in the database
+  //In the previous step the user must save the query and the querry response from the initial query to the db
+  //The query - req.body.query
+  //The response of the query (queryResponse) - res.locals.queryResponse
+  if (res.locals.cache === null) {
+    const query = req.body.query;
+    const queryResponse = res.locals.queryResponse;
 
-// client.set("foo", "bar");
-// client.set("foo2", "bar2");
-// client.set("foo3", "bar3");
-// client.set("foo4", "bar4");
-
-// client.del("foo3");
-// client.del("foo4");
-
-// client.get("foo", function(err, response) {
-//   if (err) {
-//     throw err;
-//   } else {
-//     console.log(response);
-//   }
-// });
-
-// client.keys("*", (err, res) => {
-//   if (err) {
-//     throw err;
-//   } else {
-//     console.log(res);
-//   }
-// });
-
-// client.get("", (err, response) => {
-//   if (err) {
-//     throw err;
-//   } else {
-//     console.log(response);
-//   }
-// });
+    // Stringifies the queryResponse to be able to save deeply nested objects in redis
+    client.SETEX(query, timeToLive, JSON.stringify(queryResponse), function(
+      err,
+      response
+    ) {
+      if (err) {
+        throw err;
+      } else {
+        res.locals.cache = queryResponse;
+        return next();
+      }
+    });
+  }
+  // The query is in the cache and skips over cachify
+  else {
+    return next();
+  }
+};
 
 module.exports = cacheQL;
