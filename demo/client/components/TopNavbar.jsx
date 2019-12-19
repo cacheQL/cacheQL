39b98d76376e6394b44f8f7302fdd82bb87b5916
evanking11 <evanking11@gmail.com>
2019-12-18 16:43:46 -0800

    visual changes brad

--- demo/client/components/TopNavbar.jsx
+++ demo/client/components/TopNavbar.jsx
@@ -14,14 +14,13 @@ const TopNavbar = () => {
   return (
     <div>
       <Navbar bg="light" expand="lg">
-<<<<<<< HEAD
         <img
           src="/assets/logo_transparent_2.png"
-          width="100"
+          width="90"
           height="80"
           href="#home"
           alt="logo"
-          className="d-inline-block align-top"
+          class="d-inline-block align-top"
         />
         {/* <Navbar.Brand href="#home">CacheQL</Navbar.Brand> */}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -39,25 +38,6 @@ const TopNavbar = () => {
           </Form>
         </Navbar.Collapse>
       </Navbar>
-=======
-      <img src="/assets/logo_transparent_2.png" width="90" height="80" href="#home" alt="logo" class="d-inline-block align-top"/>
-      {/* <Navbar.Brand href="#home">CacheQL</Navbar.Brand> */}
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#Team">Demo</Nav.Link>
-          <Nav.Link href="#Team">Download</Nav.Link>
-          <Nav.Link href="#Team">Team</Nav.Link>
-        </Nav>
-        <Form inline>
-        <a href="https://github.com" target="_blank">
-          <FontAwesomeIcon id="github-icon" icon={faGithub}  size="3x"/> 
-        </a>
-        </Form>
-      </Navbar.Collapse>
-    </Navbar> 
->>>>>>> dece1821a7e17a5aebc45e2f2f93b724b7fba699
     </div>
   );
 };
