e7139b97b2693324b8495048b702187f3c1f8f6a
evanking11 <evanking11@gmail.com>
2019-12-19 11:10:20 -0800

    style changes, navbar links fixed, scroll to top button added

--- demo/client/components/TopNavbar.jsx
+++ demo/client/components/TopNavbar.jsx
@@ -5,10 +5,17 @@ import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
+import { Link as LinkScroll, animateScroll } from "react-scroll";
 
 //font awesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { relative } from "path";
+
+let navStyle = {
+  width: "30%",
+  position: relative
+};
 
 const TopNavbar = () => {
   return (
@@ -25,11 +32,21 @@ const TopNavbar = () => {
         {/* <Navbar.Brand href="#home">CacheQL</Navbar.Brand> */}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#Team">Demo</Nav.Link>
-            <Nav.Link href="#Team">Download</Nav.Link>
-            <Nav.Link href="#Team">Team</Nav.Link>
+          <Nav className="mr-auto" style={navStyle}>
+            <div className="navItems">
+              <LinkScroll to="github-icon" smooth={true} offset={-64}>
+                Home
+              </LinkScroll>
+              <LinkScroll to="query-container" smooth={true} offset={-64}>
+                Demo
+              </LinkScroll>
+              <LinkScroll to="download" smooth={true} offset={-64}>
+                Download
+              </LinkScroll>
+              <LinkScroll to="TeamBox" smooth={true} offset={-64}>
+                Team
+              </LinkScroll>
+            </div>
           </Nav>
           <Form inline>
             <a href="https://github.com/cacheql/cacheql" target="_blank">
