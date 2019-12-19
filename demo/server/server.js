9900dbd1852da848273d65bf960e1a03dcdf4f02
JoshuaManto <joshuamanto@yahoo.com>
2019-12-19 12:31:42 -0800

    Removed extra anonymous middleware for testing

--- demo/server/server.js
+++ demo/server/server.js
@@ -33,11 +33,6 @@ app.get("/", (req, res) => {
 
 app.use(
   "/graphql",
-  (req, res, next) => {
-    console.log("in here SERVER");
-    console.log(req.body);
-    return next();
-  },
   graphqlHTTP({
     schema: schema,
     rootValue: resolvers,
