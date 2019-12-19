550ed8f050658665a10aa0c086a206a2322e8a42
JoshuaManto <joshuamanto@yahoo.com>
2019-12-19 12:29:58 -0800

    Refactored to use Person schema

--- demo/server/model.js
+++ demo/server/model.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const URI =
+const myURI =
   "mongodb+srv://admin:admin@cluster0-zvnqd.mongodb.net/test?retryWrites=true&w=majority";
+
+const URI = process.env.MONGO_URI || myURI;
 
 mongoose
   .connect(URI, {
@@ -10,18 +12,18 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
     // sets the name of the DB that our collections are part of
-    dbName: "cacheql"
+    dbName: "graphql-practice-codesmith"
   })
   .then(() => console.log(`Connected to Mongo DB`))
   .catch(err => console.log(err));
 
-const PersonSchema = new Schema({
-  name: { type: String, required: true },
-  message: { type: String, required: true }
+const personSchema = new Schema({
+  name: String,
+  age: Number,
+  birthdate: Date,
+  position: String
 });
 
-const test = mongoose.model("tests", PersonSchema);
+const Person = mongoose.model("people", personSchema);
 
-module.exports = {
-  test
-};
+module.exports = { Person };
