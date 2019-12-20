100ee09a13e9ab3e020e89d01403fed86bb30842
JoshuaManto <joshuamanto@yahoo.com>
2019-12-20 14:32:23 -0800

    Removed demo folder

--- demo/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-import App from "./client/App.jsx";
-import "./public/styles.scss";
-
-render(<App />, document.getElementById("root"));
