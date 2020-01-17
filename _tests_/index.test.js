df9e451b4857327dcc5d26e8bd848c20de3dbc54
bmwoolf <bradleymwoolf@gmail.com>
2020-01-17 15:39:17 -0800

    added tests for set method

--- _tests_/index.test.js
+++ _tests_/index.test.js
@@ -7,19 +7,27 @@ const redis = require('redis');
 const cacheql = require('../cacheql');
 const {auth, set, checkify, cachify} = require('../cacheql')
 
-//mauybe we just rebuild the one in cacheql
-jest.spyOn(redis, 'createClient').mockImplementation(red_mock.createClient);
-
-describe("Authentication function", () => {
-    it("should log into redis successfully", () => {
-        const credentials = {
-            port: 2343,
-
-        } //insert our actual credentials
-        expect(auth(credentials)).toEqual(true) //connected to redis cache 
-    })
+//set
+test('it should log into redis successfully', () => {
+    const testData = {
+        redisHost: "redis-10212.c52.us-east-1-4.ec2.cloud.redislabs.com",
+        redisPort: 10212,
+        redisAuth: "eRQFVq70CXuDEoISTvKNVFtdevWabNbe",
+        timeToLive: 300
+      };
+      console.log('cacheql', cacheql)
+    expect(cacheql.set(testData)).toBe(true)
 })
 
-describe('cacheql', () => {
-    it('should pass', () => {expect(true).toBe(true)})
-  })
+test('it should log into redis unsuccessfully', () => {
+    const testData = {
+        redisHost: null,
+        redisPort: null,
+        redisAuth: null,
+        timeToLive: null
+      };
+      console.log('cacheql', cacheql)
+    expect(cacheql.set(testData)).toBe(undefined)
+})
+
+//auth
